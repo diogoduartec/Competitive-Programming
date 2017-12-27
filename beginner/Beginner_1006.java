8b9fa327730dea20d325cb48ebd8709f29bbbbc6
Diogo Duarte <31852339+CostaDiogo@users.noreply.github.com>
2017-12-27 10:51:13 -0400

    Update and rename beginner_1006.java to Beginner_1006.java

--- beginner/Beginner_1006.java
+++ beginner/Beginner_1006.java
@@ -3,7 +3,7 @@ package beginner;
 import java.io.*;
 import java.util.*;
 import java.lang.*;
-public class beginner_1006 {
+public class Beginner_1006 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         double a = scan.nextDouble();

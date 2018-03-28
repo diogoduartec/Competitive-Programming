9dbc35e68c23c4376dd1ffa55e16550efb431263
Diogo Duarte <costadiogo98@outlook.com>
2018-03-28 10:44:14 -0400

    GAPA Fenwick Tree

--- Online_Judge-Solutions/Spoj/UPDATEIT.cpp
+++ Online_Judge-Solutions/Spoj/UPDATEIT.cpp
@@ -1,6 +1,8 @@
 /*
-* create by Diogo Duarte on 26/03/2018
-* Fenwick Tree implementation
+* create by Diogo Duarte on 28/03/2018
+* Problem: Update the array ! (UPDATEIT)
+* OJ: Spoj
+* # Fenwick Tree
 */
 #include <bits/stdc++.h>
 using namespace std;
@@ -8,15 +10,19 @@ using namespace std;
 #define vi vector<int>
 #define i64 long long
 #define lsb(x) (x)&(-(x))
+#define _ ios_base::sync_with_stdio(0);
 
 vi f3;
 int n;
 
 void update(int startIdx, int endIdx, i64 val){
-	while(startIdx<=endIdx){
+	while(startIdx<=n){
 		f3[startIdx]+=val;
-		cout << startIdx <<"  ";
 		startIdx += lsb(startIdx);
+	}
+	while(endIdx<=n){
+		f3[endIdx] -= val;
+		endIdx += lsb(endIdx);
 	}
 }
 
@@ -29,24 +35,22 @@ i64 query(int idx){
 	return result;
 }
 
-int main(){
+int main(){_
 	int t, u, l, r, q, idx;
 	i64 val;
 	cin >> t;
+	
 	while(t--){
 		cin >> n >> u;
 		f3.assign(n+1, 0);
+		
 		while(u--){
 			cin >> l >> r >> val;
-			update(l+1,r+1,val);
-			cout << endl << endl;
-			/*for(int i=1; i<=n; i++)
-				cout <<i<<"---"<< query(i) <<endl;
-			cout << "\n-----------------------------\n";*/
+			update(l+1,r+2,val);
 		}
-		/*for(int i=1; i<=n; i++)
-			cout <<i<<"---"<< query(i) <<endl;*/
+
 		cin >> q;
+		
 		while(q--){
 			cin >> idx;
 			cout << query(idx+1) << endl;

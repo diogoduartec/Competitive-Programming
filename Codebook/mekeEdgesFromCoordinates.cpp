0765a03bd6f73ef24d5a81f8a7688c618e0a85e7
Diogo Duarte <costadiogo98@outlook.com>
2018-05-03 11:10:49 -0400

    function to make edges from coordinates

---
+++ Codebook/mekeEdgesFromCoordinates.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ii pair<int, int >
+#define vii vector<ii>
+#define pb push_back
+#define ft first
+#define sc second
+
+typedef struct{
+	int u, v;
+	double w;
+}Edge;
+
+int n;
+vii coordinates;
+vector<Edge> edges;
+
+void makeEdges(){
+	for(int i=0; i<n; i++){
+		for(int j=i; j<n; j++){
+			double w = hypot(abs(coordinates[i].ft - coordinates[j].ft), abs(coordinates[i].sc - coordinates[j].sc));
+			Edge edge;
+			edge.u = i; edge.v = j; edge.w = w;
+			edges.pb(edge);
+		}
+	}
+}
+
+int main(){
+	cout << fixed << setprecision(2);
+	int x, y;
+	cin >> n;
+	for(int i=n;i--;){
+		cin >> x >> y;
+		coordinates.push_back({x,y});
+	}
+	makeEdges();
+	for(int i=0; i<edges.size(); i++){
+		cout << edges[i].u << " " << edges[i].v << " " << edges[i].w << endl;
+	}
+}
